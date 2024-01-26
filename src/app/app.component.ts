c182c9dc847789cb89f2180e83897f78dde72c4e
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2024-01-26 16:40:24 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CaedComponent } from './componentes/card/caed.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { TablasComponent } from './componentes/vistas/tablas/tablas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CaedComponent, FooterComponent, TablasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
